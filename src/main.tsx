52323f15ac1b2da44c505ca00e6bbda4e11b4dcb
Nora Know <aluphire.xp@gmail.com>
2025-06-23 22:12:58 -0400

    authenticator fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
+// @ts-ignore
 import outputs from "../amplify_outputs.json";
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
