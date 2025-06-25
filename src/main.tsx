284d13a3663837453232ce3695382a1e49e10c31
Nora Know <aluphire.xp@gmail.com>
2025-06-25 13:26:50 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-// @ts-ignore
 import outputs from "../amplify_outputs.json";
-import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <App />
   </React.StrictMode>
 );
