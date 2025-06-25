96f00e1c638c9d91fd44f7de24fc36c1a002243d
Nora Know <aluphire.xp@gmail.com>
2025-06-25 13:24:57 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import KnowNora from './components/KnowNora.tsx';
 
 const client = generateClient<Schema>();
 
 function App() {
-  const { signOut } = useAuthenticator();
 
   return (
     <>
-      <button onClick={signOut}>Sign out</button>
+      <KnowNora />
     </>
   );
 }
